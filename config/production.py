64fe58893c495cf7cc00a2a13ef213ad84b7be69
james <consultorjamissonfreitas@gmail.com>
2019-11-01 05:49:53 -0300

    fix app

--- config/production.py
+++ config/production.py
@@ -3,11 +3,10 @@ from decouple import config
 
 DEBUG = False
 
-# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
-#    user=config('POSTGRES_USER'),
-#    pw=config('POSTGRES_PW'),
-#    url=config('POSTGRES_URL'),
-#    db=config('POSTGRES_DB')
-#)
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=config('POSTGRES_USER'),
+    pw=config('POSTGRES_PW'),
+    url=config('POSTGRES_URL'),
+    db=config('POSTGRES_DB')
+)
 
-SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
